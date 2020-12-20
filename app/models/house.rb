48307f1c05d0538e7b2ba9f57a4be850cb8b386f
minoru mikoda <minoru.mikoda@gmail.com>
2020-12-20 07:43:53 +0000

    アソシエーションとバリデーション設定

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,2 +1,8 @@
 class House < ApplicationRecord
+  validates :name, presence: true
+  validates :rent, presence: true
+  validates :address, presence: true
+  validates :age, presence: true
+  has_many :stations
+  accepts_nested_attributes_for :stations
 end
